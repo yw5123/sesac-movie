06b075505f50045e9bf4ad91e8d6107e790c074d
yw5123 <onestar5123@naver.com>
2024-11-20 20:58:23 +0900

    hotfix: API 키 노출되어 재발급 후 수정
    - 원인: 초반 API 연결하는 과정에서 path 문제로 진행이 안 되어 여러 시도를 해보다가 API키를 직접 입력한 후 까먹음(...)
    - 해결 방법: API 키를 재발급 받고 .env 파일에서 API 키 수정

--- my-movie/src/api/axios.js
+++ my-movie/src/api/axios.js
@@ -5,7 +5,7 @@ const API_KEY = import.meta.env.VITE_TMDB_KEY;
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "0c51db4a1d2545cbd3802b020010a49b",
+    api_key: API_KEY,
     language: "ko",
   }
 });

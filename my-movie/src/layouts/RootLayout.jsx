a8a1b79b56e382116ec86a28d2d89cd8a48506f6
yw5123 <onestar5123@naver.com>
2024-11-20 18:54:51 +0900

    fix: 페이지 이동 시 스크롤 위치 오류 해결
    - 원인: RouterProvider를 사용해서 SPA(Single Page Application)이기 때문에 페이지가 새로 로드되지 않아 스크롤 위치가 유지
    - 해결 방법: window.scrollTo() 함수 등을 사용해서 수동으로 스크롤 위치를 관리해야 함
    
    => ScrollToTop 컴포넌트 추가

--- my-movie/src/layouts/RootLayout.jsx
+++ my-movie/src/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/layout/Header";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function RootLayout() {
   return (
     <>
+      <ScrollToTop />
       <Header></Header>
       <Outlet></Outlet>
     </>
